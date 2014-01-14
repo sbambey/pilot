10c0404e4b840921ce4f5972a1bec7a223046fd9
Simon Bambey <sbambey.lp@gmail.com>
2014-01-14 09:02:48 +0100

    Modified static pages

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ group :test do
   gem 'growl', '1.0.3'
 end
 
-gem 'bootstrap-sass', '2.3.2.0'
+gem 'bootstrap-sass', '3.0.3.0'
 gem 'sass-rails', '4.0.1'
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.1'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Pilot') }
+    it { should have_title(full_title('')) }
+    #it { should_not have_title('| Home') }
+  end
+
+end
