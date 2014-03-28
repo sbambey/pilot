2e261ec4709220b8ec4b16d558c9b3fd1bbd2564
Simon Bambey <sbambey.lp@gmail.com>
2014-03-28 18:20:19 +0100

    Fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ PilotApp::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  match '/addairports', to: 'airports#new',     via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
