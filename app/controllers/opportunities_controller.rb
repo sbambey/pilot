b295d90ed3a3b0bfe1e888a09f84861bde5b6952
Simon Bambey <sbambey.lp@gmail.com>
2014-06-17 18:31:27 -0700

    Finished new opportunity page

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -14,7 +14,6 @@ class OpportunitiesController < ApplicationController
     op = opportunities_params
     op['airline'] = Airline.where(airline_name: op['airline']).first
     @opportunity = Opportunity.new(op)
-    lala
   	if @opportunity.save
   		flash[:success] = "Added opportunity successfully!"
   		redirect_to @opportunity
@@ -30,7 +29,7 @@ class OpportunitiesController < ApplicationController
   private
 
   	def opportunities_params
-  		params.require(:opportunity).permit(:airline, :opportunity_name, {:website => []}, :rating).tap do |whitelisted|
+  		params.require(:opportunity).permit(:airline, :opportunity_name, :program_type, :duration, :post_graduation, {:website => []}, :hiring_status, :rating).tap do |whitelisted|
         whitelisted[:headings_attributes] = params[:opportunity][:headings_attributes]
       end
   	end
