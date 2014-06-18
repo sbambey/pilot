eb9f681566484596a08c386e8923d76a1e7437b3
Simon Bambey <sbambey.lp@gmail.com>
2014-06-17 18:46:37 -0700

    Precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,37 +16,4 @@
 //= require_tree .
 //= require bootstrap
 $(document).ready(function() {
-	$('a[id^="add-form-"]').click(function(event) {
-		clone = $(this).parent().children(".bond").first().clone(true);
-		clone.find(":input").each(function(){
-			$(this).val('');
-		});
-		$(this).parent().append(clone);
-		$(this).parent().append('<span class="help-block"></span>');
-	  event.preventDefault();
-	});
-
-	$('a[id^="add-heading"]').click(function(event) {
-		clone = $(this).parent().clone(true);
-		clone.find(":input").each(function(){
-			$(this).val('');
-		});
-		clone.append('<span class="help-block"></span>');
-		$(this).parent().parent().append(clone);
-	  event.preventDefault();
-	});
-
-	$('a[id^="add-para"]').click(function(event) {
-		clone = $(this).parent().children("[id$=_para]").first().clone(true);
-		clone.val('');
-		clone.append('<span class="help-block"></span>');
-		$(this).parent().append(clone);
-	  event.preventDefault();
-	});
-
-	$('.remove-bond').click(function(event) {
-		if($(this).parent().parent().children('.bond').length != 1)
-			$(this).closest(".bond").remove();
-		event.preventDefault();
-	});
 });
